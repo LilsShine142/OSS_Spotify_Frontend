2e3e69d84f2549ff46257ce830a366cd28503424
PhamThanhSu <psu95228@gmail.com>
2025-05-20 11:26:27 +0700

    xoa spotifyAuthService trong constants

--- src/lib/constaints/constants.jsx
+++ src/lib/constaints/constants.jsx
@@ -1,9 +1,3 @@
-export const spotifyAuthService = {
-  SPOTIFY_AUTH_URL: "https://accounts.spotify.com/api/token",
-  CLIENT_ID: "0f0a1dccc2bf4ca5b6804c566ba883cb",
-  CLIENT_SECRET: "fbaf824a59394f59af986ffea83431c6",
-};
-
 // Các cột cho bảng danh sách bài hát
 export const Trackcolumns = [
   { label: "#", span: 1, align: "center" },
