8c4ef655b37db93f7f4aefaf5ebaa2941832de03
alixblu <alixblu3011@gmail.com>
2025-05-20 21:30:15 +0700

    ws server

--- vite.config.js
+++ vite.config.js
@@ -23,10 +23,13 @@ export default defineConfig({
         secure: false,
       },
       '/ws': {
-        target: 'ws://127.0.0.1:8001',
+        target: 'http://localhost:8001',
         ws: true,
         changeOrigin: true,
-        secure: false,
+      },
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
       }
     }
   },
